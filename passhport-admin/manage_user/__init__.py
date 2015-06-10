af50c8468313b5d5fdf3e3dfe138bfb6e6bb41cb
VeryTastyTomato <VeryTastyTomato@users.noreply.github.com>
2015-06-10 17:26:17 +0200

    First connection between passhportd and passhport-admin (user creation)

---
+++ passhport-admin/manage_user/__init__.py
@@ -0,0 +1,25 @@
+# -*-coding:Utf-8 -*-
+
+"""Contains functions which make requests to the server for user management"""
+
+import requests
+
+url_passhport = "http://127.0.0.1:5000/"
+
+def requests_user_create(username, email, comment, sshkey):
+    """User creation on passhportd via API"""
+
+    user_data = {'username': username, 'email': email, 'comment': comment, 'sshkey': sshkey}
+    url_create = url_passhport + "user/create"
+
+    try:
+        r = requests.post(url_create, data = user_data)
+    except requests.RequestException, e:
+        print("ERROR: " + str(e.message))
+    else:
+        print(r.text)
+
+        if r.status_code == requests.codes.ok:
+            return 0
+
+    return 1
