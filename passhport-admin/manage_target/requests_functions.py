5eaa3770a6701082f1921a9f495b19d18333d86b
VeryTastyTomato <ibrar.asghar@librit.fr>
2015-06-15 01:19:45 +0200

    Reorganisation of client’s modules code and rewriting in more “pythonic” code

---
+++ passhport-admin/manage_target/requests_functions.py
@@ -0,0 +1,130 @@
+# -*-coding:Utf-8 -*-
+
+"""Contains functions which make requests to the server for target’s management"""
+
+import requests
+
+url_passhport = "http://127.0.0.1:5000/"
+
+##### Target #####
+def requests_target_list():
+    """Get the list of all targets"""
+
+    url_list = url_passhport + "target/list"
+
+    try:
+        r = requests.get(url_list)
+    except requests.RequestException, e:
+        print("ERROR: " + str(e.message))
+    else:
+        print(r.text)
+
+        if r.status_code == requests.codes.ok:
+            return 0
+
+    return 1
+
+def requests_target_search(pattern):
+    """Get the list of targets that match the pattern"""
+
+    url_search = url_passhport + "target/search/" + pattern
+
+    try:
+        r = requests.get(url_search)
+    except requests.RequestException, e:
+        print("ERROR: " + str(e.message))
+    else:
+        print(r.text)
+
+        if r.status_code == requests.codes.ok:
+            return 0
+
+    return 1
+
+def requests_target_create(targetname, hostname, comment, sshoptions, port, servertype, autocommand):
+    """Target creation on passhportd via API"""
+
+    target_data = {'targetname': targetname, 'hostname': hostname, 'comment': comment, 'sshoptions': sshoptions, 'port': port, 'servertype': servertype, 'autocommand': autocommand}
+    url_create = url_passhport + "target/create"
+
+    try:
+        r = requests.post(url_create, data = target_data)
+    except requests.RequestException, e:
+        print("ERROR: " + str(e.message))
+    else:
+        print(r.text)
+
+        if r.status_code == requests.codes.ok:
+            return 0
+
+    return 1
+
+def requests_target_show(targetname):
+    """Get data about a target"""
+
+    url_show = url_passhport + "target/show/" + targetname
+
+    try:
+        r = requests.get(url_show)
+    except requests.RequestException, e:
+        print("ERROR: " + str(e.message))
+    else:
+        print(r.text)
+
+        if r.status_code == requests.codes.ok:
+            return 0
+
+    return 1
+
+def requests_target_edit(targetname, new_targetname, new_hostname, new_comment, new_sshoptions, new_port, new_servertype, new_autocommand):
+    """Target modification on passhportd via API"""
+
+    target_data = {'targetname': targetname, 'new_targetname': new_targetname, 'new_hostname': new_hostname, 'new_comment': new_comment, 'new_sshoptions': new_sshoptions, 'new_port': new_port, 'new_servertype': new_servertype, 'new_autocommand': new_autocommand}
+    url_edit = url_passhport + "target/edit/"
+
+    try:
+        r = requests.post(url_edit, data = target_data)
+    except requests.RequestException, e:
+        print("ERROR: " + str(e.message))
+    else:
+        print(r.text)
+
+        if r.status_code == requests.codes.ok:
+            return 0
+
+    return 1
+
+def requests_target_del(targetname):
+    """Target deletion on passhportd via API"""
+
+    url_del = url_passhport + "target/del/" + targetname
+
+    try:
+        r = requests.get(url_del)
+    except requests.RequestException, e:
+        print("ERROR: " + str(e.message))
+    else:
+        print(r.text)
+
+        if r.status_code == requests.codes.ok:
+            return 0
+
+    return 1
+
+def requests_target_adduser(email, targetname):
+    """Adding a user in a target via API"""
+
+    target_user_data = {'targetname': targetname, 'email': email}
+    url_adduser = url_passhport + "target/adduser"
+
+    try:
+        r = requests.post(url_adduser, data = target_user_data)
+    except requests.RequestException, e:
+        print("ERROR: " + str(e.message))
+    else:
+        print(r.text)
+
+        if r.status_code == requests.codes.ok:
+            return 0
+
+    return 1
--- passhportd/app/views.py
+++ passhportd/app/views.py
@@ -4,4 +4,3 @@ from .views_mod import user, usergroup, target, targetgroup
 @app.route('/')
 def imalive():
         return """Passhport is running, gratz!"""
-
