d0a398312ccc9b1bb9581bd95df6385c3de31a92
Raphaël Berlamont <raphux@localhost.localdomain>
2017-01-23 20:02:19 +0100

    Added a script which is aimed to be run in a docker environment

---
+++ scripts_utils/launch_passhportd_docker_v0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+VIRTUAL_ENV_PYTHON="$1"
+
+# Generate keys if they don't exist
+if [ ! -r "/home/passhport/.ssh/id_rsa" ]
+then
+	/usr/bin/ssh-keygen -t rsa -b 4096 -N "" -f "/home/passhport/.ssh/id_rsa"
+fi
+
+if [ ! -r "/home/passhport/.ssh/id_ecdsa" ]
+then
+	/usr/bin/ssh-keygen -t ecdsa -b 521 -N "" -f "/home/passhport/.ssh/id_ecdsa"
+fi
+
+
+# Launch the passhportd in the virtualenv
+"${VIRTUAL_ENV_PYTHON}" /home/passhport/passhport/passhportd/passhportd >> /var/log/passhport/passhportd &
