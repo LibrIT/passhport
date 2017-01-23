3b9e3df0e431468a5accc01c38515e1cdf201607
Raphaël Berlamont <raphael.berlamont@raphux.com>
2017-01-23 20:22:29 +0100

    Modify ownership and rights of /home/passhport/.ssh when passhport is launched by docker

--- scripts_utils/launch_passhportd_docker_v0.sh
+++ scripts_utils/launch_passhportd_docker_v0.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 VIRTUAL_ENV_PYTHON="$1"
 
+# Change ownership and rights because it's an external volume mounted by docker
+chown -R passhport:passhport /home/passhport/.ssh/
+chmod 700 /home/passhport/.ssh/
+
 # Generate keys if they don't exist
 if [ ! -r "/home/passhport/.ssh/id_rsa" ]
 then
