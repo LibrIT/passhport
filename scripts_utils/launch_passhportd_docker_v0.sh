fdf8aec81d85e1274c2865faf8db948847d240f4
Raphaël Berlamont <raphael.berlamont@raphux.com>
2017-01-23 20:31:02 +0100

    Docker script Need to be launched by root, so I added some "su - passhport" stuffs

--- scripts_utils/launch_passhportd_docker_v0.sh
+++ scripts_utils/launch_passhportd_docker_v0.sh
@@ -8,14 +8,15 @@ chmod 700 /home/passhport/.ssh/
 # Generate keys if they don't exist
 if [ ! -r "/home/passhport/.ssh/id_rsa" ]
 then
-	/usr/bin/ssh-keygen -t rsa -b 4096 -N "" -f "/home/passhport/.ssh/id_rsa"
+	su - passhport -c "/usr/bin/ssh-keygen -t rsa -b 4096 -N \"\" -f \"/home/passhport/.ssh/id_rsa\""
 fi
 
 if [ ! -r "/home/passhport/.ssh/id_ecdsa" ]
 then
-	/usr/bin/ssh-keygen -t ecdsa -b 521 -N "" -f "/home/passhport/.ssh/id_ecdsa"
+	su - passhport -c "/usr/bin/ssh-keygen -t ecdsa -b 521 -N \"\" -f \"/home/passhport/.ssh/id_ecdsa\""
 fi
 
 
 # Launch the passhportd in the virtualenv
-"${VIRTUAL_ENV_PYTHON}" /home/passhport/passhport/passhportd/passhportd
+su - passhport -c "\"${VIRTUAL_ENV_PYTHON}\" /home/passhport/passhport/passhportd/passhportd"
+
