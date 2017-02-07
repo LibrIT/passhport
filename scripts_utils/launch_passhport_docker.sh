1bb1eafb3c9ffd62f3c497ec7547cc24eb43e84e
Raphaël Berlamont <raphael.berlamont@raphux.com>
2017-02-08 00:44:02 +0100

    Created a launch script for the passhport docker. Removed sed from docker file to include it into the launch_script.

---
+++ scripts_utils/launch_passhport_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "What is the hostname of your passhportd host ?"
+read PASSHPORTD_HOSTNAME
+
+echo "172.17.0.1 ${PASSHPORTD_HOSTNAME}" >> /etc/hosts
+
+sed -ie 's#url_passhport =.*#url_passhport = "https://${PASSHPORTD_HOSTNAME}:5000/"#' /home/passhport/passhport/passhport/passhport
+
+/usr/sbin/sshd -D
