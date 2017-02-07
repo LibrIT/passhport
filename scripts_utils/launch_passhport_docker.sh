a17517427ded828fb9836df6babcfebc7bdd0518
Raphaël Berlamont <raphael.berlamont@raphux.com>
2017-02-08 00:58:39 +0100

    Corrected double quotes VS quote mis-usage.

--- scripts_utils/launch_passhport_docker.sh
+++ scripts_utils/launch_passhport_docker.sh
@@ -5,6 +5,7 @@ read PASSHPORTD_HOSTNAME
 
 echo "172.17.0.1 ${PASSHPORTD_HOSTNAME}" >> /etc/hosts
 
-sed -ie 's#url_passhport =.*#url_passhport = "https://${PASSHPORTD_HOSTNAME}:5000/"#' /home/passhport/passhport/passhport/passhport
+sed -ie "s#url_passhport =.*#url_passhport = \"https://${PASSHPORTD_HOSTNAME}:5000/\"#" /home/passhport/passhport/passhport/passhport
 
+echo "Launching sshd daemon…"
 /usr/sbin/sshd -D
