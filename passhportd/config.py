45c8b5342ba0f4a9cabaecf832432bd57d797e7d
Raphaël Berlamont <raphael.berlamont@raphux.com>
2018-02-13 21:53:05 +0100

    passhportd : set default parameters for LDAP section

--- passhportd/config.py
+++ passhportd/config.py
@@ -18,36 +18,36 @@ else:
 
 # Reading the configuration
 """Database (SQLite by default)"""
-SQLALCHEMY_TRACK_MODIFICATIONS = conf.getboolean("Database", \
-                                "SQLALCHEMY_TRACK_MODIFICATIONS")
-SQLALCHEMY_DATABASE_DIR = conf.get("Database", \
-                                "SQLALCHEMY_DATABASE_DIR")
-SQLALCHEMY_DATABASE_URI = conf.get("Database", \
-                                "SQLALCHEMY_DATABASE_URI")
-SQLALCHEMY_MIGRATE_REPO = conf.get("Database", \
-                                "SQLALCHEMY_MIGRATE_REPO")
+SQLALCHEMY_TRACK_MODIFICATIONS  = conf.getboolean("Database", \
+                                    "SQLALCHEMY_TRACK_MODIFICATIONS")
+SQLALCHEMY_DATABASE_DIR         = conf.get("Database", \
+                                    "SQLALCHEMY_DATABASE_DIR")
+SQLALCHEMY_DATABASE_URI         = conf.get("Database", \
+                                    "SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_MIGRATE_REPO         = conf.get("Database", \
+                                    "SQLALCHEMY_MIGRATE_REPO")
 """ SSH Keyfile """
-SSH_KEY_FILE = conf.get("Environment", "SSH_KEY_FILE")
+SSH_KEY_FILE    = conf.get("Environment", "SSH_KEY_FILE")
 
 """ PaSSHport path """
-PASSHPORT_PATH = conf.get("Environment", "PASSHPORT_PATH")
-PYTHON_PATH = conf.get("Environment", "PYTHON_PATH")
+PASSHPORT_PATH  = conf.get("Environment", "PASSHPORT_PATH")
+PYTHON_PATH     = conf.get("Environment", "PYTHON_PATH")
 
 """ Server configuration """
-HOST =  conf.get("Network", "LISTENING_IP")
+HOST            =  conf.get("Network", "LISTENING_IP")
 
 """ LDAP configuration """
-LDAPURI = conf.get("LDAP", "LDAP_PROVIDER_URL")
-LDAPPORT = int(conf.get("LDAP", "LDAP_PORT"))
-LDAPBASE = conf.get("LDAP", "LDAP_USER_BASEDN")
-LDAPFIELD = conf.get("LDAP", "LDAP_LOGIN_SEARCH_FIELD")
-LDAPACC = conf.get("LDAP", "LDAP_ACC")
-LDAPPASS = conf.get("LDAP", "LDAP_PASS")
+LDAPURI         = conf.get("LDAP", "LDAP_PROVIDER_URL", fallback='undefined')
+LDAPPORT        = conf.getint("LDAP", "LDAP_PORT", fallback='389')
+LDAPBASE        = conf.get("LDAP", "LDAP_USER_BASEDN", fallback='undefined')
+LDAPFIELD       = conf.get("LDAP", "LDAP_LOGIN_SEARCH_FIELD", fallback='undefined')
+LDAPACC         = conf.get("LDAP", "LDAP_ACC", fallback='undefined')
+LDAPPASS        = conf.get("LDAP", "LDAP_PASS", fallback='undefined')
 
 """ SSL Configuration """
-SSL            = conf.get("SSL", "SSL")
-SSL_CERTIFICAT = conf.get("SSL", "SSL_CERTIFICAT")
-SSL_KEY        = conf.get("SSL", "SSL_KEY")
+SSL             = conf.get("SSL", "SSL")
+SSL_CERTIFICAT  = conf.get("SSL", "SSL_CERTIFICAT")
+SSL_KEY         = conf.get("SSL", "SSL_KEY")
 
 
 
