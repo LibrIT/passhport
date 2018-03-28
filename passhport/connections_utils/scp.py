a0ea42e38b3e51ed71006bcb79fff41129550dc1
Erwan Le Gall <legall.erwan@gmail.com>
2018-03-28 12:02:54 +0200

    Now possible to scp via a target to another server

--- passhport/connections_utils/scp.py
+++ passhport/connections_utils/scp.py
@@ -26,30 +26,63 @@ def parse(originalcmd):
     # and we should obtain the target name a line like
     # scp -t /path/on/destination/file 
     # scp -f /path/on/destination/file /local/path
-    target = re.findall("([^\s]*)//", originalcmd)
-    if target:
-        target = target[0]
+    nextuser = ""
+    nexttarget = ""
+    commandelts = re.split("//", originalcmd)
+    target      = re.split(" ", commandelts[0])[-1]
+    path        = "/" + commandelts[-1]
+    # currently the command is "ssh -t targetname". We want "ssh -t path"
+    cmd         = " ".join(re.split(" ", commandelts[0])[:-1]) + " " + path
+    if len(commandelts) == 4:
+        # Command was like :
+        # "scp mygreatfile bastion:target//user//nextserver//path"
+        nextuser   = commandelts[1]
+        nexttarget = commandelts[2]
+    elif len(commandelts) != 2:
+        print("Syntax error: there is too many or too few \"//\"." + \
+              "It should be like: scp passhport@target//path/to/copy...")
+        return False
 
-    cmd = re.sub("[^\s]*//", "/", originalcmd)
-
-    return [target, cmd]
-    
+    return [target, cmd, path, nextuser, nexttarget]
     
 
 def connect(target, filelog, login, sshoptions, originalcmd):
     """ Simply launch the scp connection """
-
     # The final command should be like
     # ssh -q -t login@target scp -t /path/to/target
     # or to receive
     # ssh -q -t login@target scp -f /path/to/target /local/path/
     # and ssh/scp do magic after that
     cmd = re.sub("scp -(.) ", "ssh -q -t " + login + "@" + target + 
-                 " scp -\\1 ", originalcmd )  
-    os.system(cmd)
-    
+            "scp -\\1 ", originalcmd)  
+
     # Print the command on a logfile
     filelog = open(filelog + "-scp.log", "a")
     filelog.write("Original command: " + originalcmd + " - " + "cmd: " + cmd)
     filelog.close()
+    # Actually execute the command
+    os.system(cmd)
+
+
+def specialconnect(target, filelog, login, sshoptions, originalcmd):
+    """Special in the case we need to retrieve a file from a server 
+       behind a target: we need to do everything in one pass"""
+    
+    # We need to put the first scp in a job (with &) because if we don't, 
+    # the next scp hangs out and the command never finish.
+    # So we wait $! to finish before launching the last scp
+    cmd = "ssh -q -t " + sshoptions + " " + login + "@" + target + " " + \
+          originalcmd[0] + " && " + \
+          "ssh -q -t " + sshoptions + " " + login + "@" + target + " " + \
+          originalcmd[1] + " & " + \
+          "wait $! && " + \
+          "ssh -q -t " + sshoptions + " " + login + "@" + target + " " + \
+          originalcmd[2] 
+
+    # Print the command on a logfile
+    filelog = open(filelog + "-scp.log", "a")
+    filelog.write("Retrive a file - " + "cmd: " + cmd)
+    filelog.close()
+    # Actually execute the command
+    os.system(cmd)
 
