7b3412e0dde8a29b595b7fc5d9e0291a76dea496
Raphaël Berlamont <raphael.berlamont@raphux.com>
2019-03-31 17:35:31 +0200

    CentOS7 install script update

--- tools/passhport-install-script-debian-8-9.sh
+++ tools/passhport-install-script-debian-8-9.sh
@@ -57,7 +57,7 @@ echo 'rm /usr/sbin/passhportd'
 echo 'rm /etc/bash_completion.d/passhport-admin'
 echo ''
 echo 'You may also remove virtualenv that has been installed by pip3 :'
-echo '# pip3 uninstall virtualenv'
+echo '# pip3.4 uninstall virtualenv'
 echo ''
 echo 'Remove the systemd service :'
 echo '# systemctl disable passhportd'
@@ -66,7 +66,7 @@ echo '# systemctl daemon-reload'
 echo ''
 echo "Finally you may also purge the following packages if you don't need them"
 echo 'anymore:'
-echo 'python3-pip git openssl (# apt purge python3-pip git openssl)'
+echo 'python3-pip, git, openssl (# apt purge python3-pip git openssl)'
 echo ''
 
 if [ ${INTERACTIVE} -eq 1 ]
@@ -95,7 +95,7 @@ apt install -y python3-pip git openssl
 echo '##############################################################'
 echo '# Installing virtualenv with pip…'
 echo '##############################################################'
-pip3 install virtualenv
+pip3.4 install virtualenv
 echo '##############################################################'
 echo '# Creating "passhport" system user'
 echo '##############################################################'
