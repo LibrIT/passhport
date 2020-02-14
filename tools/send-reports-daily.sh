bc0dcad72146d1e4ad474d286e63c564ceb64582
Erwan Le Gall <erwan.legall@librit.fr>
2020-02-14 01:05:12 +0100

    Daily report was on last 7 days

--- tools/send-reports-daily.sh
+++ tools/send-reports-daily.sh
@@ -2,5 +2,5 @@
 
 PASSHPORT_HOST_WITH_PORT=$1
 RECIPIENT=$2
-(echo To:${RECIPIENT}; echo From:passhport; echo "Content-Type: text/html"; echo Subject: PaSSHport $(date  --date="7 days ago" +%y-%m) daily report; echo; wget --no-check-certificate -qO - https://${PASSHPORT_HOST_WITH_PORT}/reporting/daily) | /usr/sbin/sendmail -t
+(echo To:${RECIPIENT}; echo From:passhport; echo "Content-Type: text/html"; echo Subject: PaSSHport $(date  --date="yesterday" +%d/%m/%Y) daily report; echo; wget --no-check-certificate -qO - https://${PASSHPORT_HOST_WITH_PORT}/reporting/daily) | /usr/sbin/sendmail -t
 
