fe1e4cde382e7aee7d36c4f9008e51b948902b9a
Raphaël Berlamont <raphael.berlamont@raphux.com>
2020-04-27 01:24:15 +0200

    WIP - PaSSHweb-Release

--- tools/passhport-install-apache2-frontend-debian.sh
+++ tools/passhport-install-apache2-frontend-debian.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 # Install Apache2
+echo 'Installing apache2 ans mod-wsgi for python3'
 apt -y install apache2 libapache2-mod-wsgi-py3
 
 # Create vhost file
+echo 'Create passhportd configuration file for apache2'
 cat <<EOF > /etc/apache2/sites-available/passhportd.conf
 Listen 5000
 <VirtualHost *:5000>
@@ -24,9 +26,17 @@ Listen 5000
 </VirtualHost>
 EOF
 
-
+echo 'Disabling default apache2 site'
 a2dissite 000-default
+
+echo 'Enabling ssl module for apache2'
 a2enmod ssl
+
+echo 'Activate passhportd configuration'
 a2ensite passhportd.conf
 
-systemctl restart apache2
+echo 'Stop current passhportd'
+systemctl stop passhportd
+
+echo 'Restarting apache2'
+systemctl restart apache2 || journalctl -xeu apache2
